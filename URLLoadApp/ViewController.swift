6eec7ba623b5bbfb3af8a6720ebbaf44854d4c7b
zino <minzino@hanmail.net>
2020-03-05 17:48:00 +0900

    Initial Commit

---
+++ URLLoadApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLLoadApp
+//
+//  Created by zino-mac on 2020/03/05.
+//  Copyright © 2020 zino-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
